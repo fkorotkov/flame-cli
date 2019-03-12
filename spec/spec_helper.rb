8cf49261c8ecd85001a804465213b1608699af6c
Alexander Popov <alex.wayfer@gmail.com>
2019-03-13 02:06:49 +0300

    Add Codecov token into Cirrus config

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,5 +1,18 @@
 # frozen_string_literal: true
 
+require 'simplecov'
+SimpleCov.start do
+	add_filter '/spec/'
+end
+SimpleCov.start
+
+if ENV['CODECOV_TOKEN']
+	require 'codecov'
+	SimpleCov.formatter = SimpleCov::Formatter::Codecov
+end
+
+require 'pry-byebug'
+
 FLAME_CLI = File.join(__dir__, '../bin/flame').freeze
 
 def transform_words_into_regexp(*words)
